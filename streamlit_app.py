1da868b1fecac8bbe4a23718f3a3e42df3ce04a2
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-20 17:17:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,12 @@
 import streamlit as st
 
+st.title('st.experimental_get_query_params')
+
+st.header('Contents of st.experimental_get_query_params')
+st.write(st.experimental_get_query_params)
+
+st.markdown('''
+In the above URL bar of your internet browser, append at the end of the URL the following:
+
+`?name=Jack&surname=Database`
+''')
