ccd614af73899d14350d06912db987f46391d41e
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-21 21:59:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,8 @@ import streamlit as st
 
 st.title('st.experimental_get_query_params')
 
-# Contents of st.experimental_get_query_params
-st.header('Contents of st.experimental_get_query_params')
-
+# Instructions
+st.header('1. Instructions')
 st.markdown('''
 In the above URL bar of your internet browser, append the following:
 
@@ -17,11 +16,13 @@ such that it becomes
 `http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/?firstname=Jack&surname=Beanstalk`
 ''')
 
+# Contents of st.experimental_get_query_params
+st.header('2. Contents of st.experimental_get_query_params')
 st.write(st.experimental_get_query_params())
 
 
 # Retrieving and displaying information from the URL
-st.header('Retrieving and displaying information from the URL')
+st.header('3. Retrieving and displaying information from the URL')
 
 firstname = st.experimental_get_query_params()['firstname'][0]
 surname = st.experimental_get_query_params()['surname'][0]
